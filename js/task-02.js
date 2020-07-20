71939eac9aae0f0c0339dd62708c378fe18deec9
Serhii Malyshko <malyshkoserhii91@gmail.com>
2020-07-20 19:18:20 +0300

    task p is added, task 2 is formatted by using prettier

--- js/task-02.js
+++ js/task-02.js
@@ -1,14 +1,13 @@
-"use strict"
+"use strict";
 
 const countProps = function (countProps) {
-    const keys = Object.keys(countProps);
-    
-    return `Кількість властивостей в об'єкті: ${keys.length}`;
-}
+  const keys = Object.keys(countProps);
 
+  return `Кількість властивостей в об'єкті: ${keys.length}`;
+};
 
 console.log(countProps({}));
 
-console.log(countProps({ name: 'Mango', age: 2 }));
+console.log(countProps({ name: "Mango", age: 2 }));
 
-console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }));
+console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }));
